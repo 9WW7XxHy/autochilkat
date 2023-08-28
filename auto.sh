8b09a3800ffe7a47506b1498ab174f6f88393eb7
admin <git@gon.so>
2023-08-29 00:31:24 +0700

    new

--- auto.sh
+++ auto.sh
@@ -1,3 +1,6 @@
+wget https://github.com/9WW7XxHy/autochilkat/blob/main/chilkat_9_5_0.so
+wget https://github.com/9WW7XxHy/autochilkat/blob/main/20-chilkat.ini
+
 extension_dir=$(php -r 'echo ini_get("extension_dir");')
 php_d_dir=$(php -i | grep 'Scan this dir for additional .ini files' | awk -F'=>' '{gsub(/^[ \t]+/, "", $2); print $2}')
 
@@ -11,6 +14,10 @@ if [ -z "$php_d_dir" ]; then
     exit 1
 fi
 
+if [ ! -f "chilkat_9_5_0.so" ] || [ ! -f "20-chilkat.ini" ]; then
+    echo "Một trong hai tệp không tồn tại."
+    exit 1
+fi
+
 cp chilkat_9_5_0.so "$extension_dir"
 cp 20-chilkat.ini "$php_d_dir"
-
