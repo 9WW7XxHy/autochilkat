857af488d3c104af31dd2879ed50873e7c170c64
admin <git@gon.so>
2023-08-29 01:44:22 +0700

    new

--- auto.sh
+++ auto.sh
@@ -19,5 +19,8 @@ if [ ! -f "chilkat_9_5_0.so" ] || [ ! -f "20-chilkat.ini" ]; then
     exit 1
 fi
 
+echo "##### push to $extension_dir "
+
 cp chilkat_9_5_0.so "$extension_dir"
+echo "######## push to $php_d_dir"
 cp 20-chilkat.ini "$php_d_dir"
